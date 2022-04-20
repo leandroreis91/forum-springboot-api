7d3143dba05ea7db9f31868f14afc9533dc30ced
Leandro Reis <leandro.reis@outlook.com>
2022-04-20 20:15:14 -0300

    Criação do primeiro endpoint para Topicos

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@Controller
+public class TopicosController {
+	
+	@RequestMapping("/topicos")
+	@ResponseBody
+	public List<Topico> lista() {
+		Topico topico = new Topico("Duvida", "Duvida com o spring", new Curso("Spring", "Programacao"));
+
+		return Arrays.asList(topico, topico, topico);
+	}
+
+}
